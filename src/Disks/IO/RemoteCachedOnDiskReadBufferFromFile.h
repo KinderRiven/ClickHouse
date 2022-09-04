a5d1e4bfd6295f8acc498ec5b8e0c992795fc082
KinderRiven <1339764596@qq.com>
2022-09-04 17:41:06 +0000

    fix

--- src/Disks/IO/RemoteCachedOnDiskReadBufferFromFile.h
+++ src/Disks/IO/RemoteCachedOnDiskReadBufferFromFile.h
@@ -56,41 +56,12 @@ public:
 
     String getFileName() const override { return source_file_path; }
 
-    enum class ReadType
-    {
-        CACHED,
-        REMOTE_FS_READ_BYPASS_CACHE,
-        REMOTE_FS_READ_AND_PUT_IN_CACHE,
-    };
-
 private:
     void initialize(size_t offset, size_t size);
 
-    ImplementationBufferPtr getImplementationBuffer(FileSegmentPtr & file_segment);
-
-    ImplementationBufferPtr getReadBufferForFileSegment(FileSegmentPtr & file_segment);
-
-    ImplementationBufferPtr getCacheReadBuffer(size_t offset) const;
-
-    std::optional<size_t> getLastNonDownloadedOffset() const;
-
-    bool updateImplementationBufferIfNeeded();
-
-    void predownload(FileSegmentPtr & file_segment);
+    size_t getTotalSizeToRead() const;
 
     bool nextImplStep();
-
-    void assertCorrectness() const;
-
-    std::shared_ptr<ReadBufferFromFileBase> getRemoteFSReadBuffer(FileSegmentPtr & file_segment, ReadType read_type_);
-
-    size_t getTotalSizeToRead();
-
-    bool completeFileSegmentAndGetNext();
-
-    void appendFilesystemCacheLog(const FileSegment::Range & file_segment_range, ReadType read_type);
-
-    bool writeCache(char * data, size_t size, size_t offset, FileSegment & file_segment);
 
     Poco::Logger * log;
     FileCache::Key cache_key;
@@ -109,27 +80,8 @@ private:
     /// Remote read buffer, which can only be owned by current buffer.
     FileSegment::RemoteFileReaderPtr remote_file_reader;
 
-    std::optional<FileSegmentsHolder> file_segments_holder;
-    FileSegments::iterator current_file_segment_it;
-
     ImplementationBufferPtr implementation_buffer;
     bool initialized = false;
-
-    ReadType read_type = ReadType::REMOTE_FS_READ_BYPASS_CACHE;
-
-    static String toString(ReadType type)
-    {
-        switch (type)
-        {
-            case ReadType::CACHED:
-                return "CACHED";
-            case ReadType::REMOTE_FS_READ_BYPASS_CACHE:
-                return "REMOTE_FS_READ_BYPASS_CACHE";
-            case ReadType::REMOTE_FS_READ_AND_PUT_IN_CACHE:
-                return "REMOTE_FS_READ_AND_PUT_IN_CACHE";
-        }
-        __builtin_unreachable();
-    }
 
     size_t first_offset = 0;
     String nextimpl_step_log_info;
@@ -141,11 +93,6 @@ private:
 
     bool allow_seeks_after_first_read;
     [[maybe_unused]]bool use_external_buffer;
-    CurrentMetrics::Increment metric_increment{CurrentMetrics::FilesystemCacheReadBuffers};
-    ProfileEvents::Counters current_file_segment_counters;
-
-    FileCache::QueryContextHolder query_context_holder;
-
     bool is_persistent;
 };
 
